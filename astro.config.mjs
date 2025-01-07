2b17a43142b1952f950fde2009de511389ab003c
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-01-07 16:14:08 -0500

    Creating i18n for multilanguage

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,19 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'CRM Documentation',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				// github: 'https://github.com/withastro/starlight',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+			defaultLocale: 'en',
+			locales: {
+				en: {
+					label: 'English',
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+				es: {
+					label: 'Español'
 				},
-			],
+			}
 		}),
 	],
 });
