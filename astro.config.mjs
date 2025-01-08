f7b8d36193d292d4261e13604837c729d1e4ec6d
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2025-01-08 09:29:13 -0500

    Integrating TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,31 @@ import { defineConfig } from 'astro/config';
 
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'CRM Documentation',
-			social: {
-				// github: 'https://github.com/withastro/starlight',
-			},
-			defaultLocale: 'en',
-			locales: {
-				en: {
-					label: 'English',
-				},
-				es: {
-					label: 'Español'
-				},
-			}
-		}),
-	],
+    integrations: [starlight({
+        title: 'CRM Documentation',
+        customCss: [
+            // Path to your Tailwind base styles:
+            './src/tailwind.css',
+        ],
+        social: {
+            // github: 'https://github.com/withastro/starlight',
+        },
+        defaultLocale: 'en',
+        locales: {
+            en: {
+                label: 'English',
+            },
+            es: {
+                label: 'Español'
+            },
+        }
+    }), tailwind({
+        // Disable the default base styles:
+        applyBaseStyles: false,
+    }),
+    ],
 });
